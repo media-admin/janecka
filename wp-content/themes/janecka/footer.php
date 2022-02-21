0a0623da2a3b3db2a640247e367085d55577d443
media-admin <markus.tritremmel@media-lab.at>
2022-02-22 00:42:55 +0100

    Added Read More Functionality

--- wp-content/themes/janecka/footer.php
+++ wp-content/themes/janecka/footer.php
@@ -168,6 +168,44 @@
 
 
 
+<!-- Read More / Read Less Functionality  -->
+<script>
+	var $el, $ps, $up, totalHeight;
+
+	jQuery(".sidebar-box .read-more__button").click(function() {
+
+		totalHeight = 0
+
+		$el = $(this);
+		$p  = $el.parent();
+		$up = $p.parent();
+		$ps = $up.find("p:not('.read-more')");
+
+		// measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
+		$ps.each(function() {
+			totalHeight += $(this).outerHeight() + 30;
+		});
+
+		$up
+			.css({
+				// Set height to prevent instant jumpdown when max height is removed
+				"height": $up.height(),
+				"max-height": 9999
+			})
+			.animate({
+				"height": totalHeight
+			});
+
+		// fade out read-more
+		$p.fadeOut();
+
+		// prevent jump-down
+		return false;
+
+	});
+</script>
+
+
 </body>
 
 </html>
