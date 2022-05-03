403762dbd0a5568dd7d6d976e2b20c86b0034d4f
media-admin <markus.tritremmel@media-lab.at>
2022-05-03 14:23:54 +0200

    Fixed GA4-Error in cookie-notice.js

--- wp-content/themes/janecka/assets/js/cookie-notice.js
+++ wp-content/themes/janecka/assets/js/cookie-notice.js
@@ -1,6 +1,4 @@
-<script>
-
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 
   dywc.init({
 
@@ -8,7 +6,7 @@
    cookie_name: 'dywc', // Name des Cookies, der zur Speicherung der Entscheidung verwendet wird
    cookie_expire: 31536e3, // Laufzeit des Cookies in Sekunden (31536e3 = 1Jahr)
    cookie_path: '/', // Pfad auf dem der Cookie gespeichert wird
-   mode: 1, // 1 oder 2, bestimmt den Buttonstil 
+   mode: 1, // 1 oder 2, bestimmt den Buttonstil
    bglayer: true, // Verdunklung des Hintegrundes aktiv (true) oder inaktiv (false)
    position: 'mt', // mt, mm, mb, lt, lm, lb, rt, rm, rb
 
@@ -58,54 +56,54 @@
 	 accept: function() {
 
    dywc.log("Load Statistic Tracking");
-		
+
 		var el = document.createElement('script');
-		el.src = 'https://www.googletagmanager.com/gtag/js?id=G-294429068';
+		el.src = 'https://www.googletagmanager.com/gtag/js?id=G-EG4NB8NX4L';
 		el.async = 1;
 		document.getElementsByTagName('head')[0].appendChild(el);
-							
+
 		window.dataLayer = window.dataLayer || [];
-						
+
 		function gtag(){dataLayer.push(arguments);}
 			gtag('js', new Date());
-							
+
 			gtag('config', 'UA-11678917-1', { 'anonymize_ip': true });
-			gtag('config', 'G-294429068', { 'anonymize_ip': true });
+			gtag('config', 'G-EG4NB8NX4L', { 'anonymize_ip': true });
 
 	 },
-	 
+
 	 reject: function() {
 
 	  // Hier kommt der Opt-Out Code rein
 	  // Folgendes Beispiel für Google Analytics
-		
+
 		var el = document.createElement('script');
-		el.src = 'https://www.googletagmanager.com/gtag/js?id=G-294429068';
+		el.src = 'https://www.googletagmanager.com/gtag/js?id=G-EG4NB8NX4L';
 		el.async = 1;
 		document.getElementsByTagName('head')[0].appendChild(el);
-						
-		window['ga-disable-G-294429068'] = true;
+
+		window['ga-disable-G-EG4NB8NX4L'] = true;
 		window.dataLayer = window.dataLayer || [];
-						
+
 		function gtag(){ dataLayer.push(arguments); }
-		
+
 		gtag('js', new Date());
-						
+
 		gtag('config', 'UA-11678917-1');
-		gtag('config', 'G-294429068');
-		
+		gtag('config', 'G-EG4NB8NX4L');
+
 		// Original Cookie Snippet
 		//
 	  // var disableStr = 'ga-disable-{TrackingID}';
-		// 
+		//
 	  // window[disableStr] = true; document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-		// 
+		//
 	  // dywc.cookie.removeItem('_ga', '/', '.domain.de');
 	  // dywc.cookie.removeItem('_gid', '/', '.domain.de');
 	  // dywc.cookie.removeItem('_gat', '/', '.domain.de');
 	  // dywc.cookie.removeItem('_gat_gtag_{TrackingID}', '/', '.domain.de');
-		
-		
+
+
 
 	 }
 	}
@@ -114,5 +112,3 @@
   });
 
   });
-  
-</script>
